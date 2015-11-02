5985ee8bd74921cb2cc8c6f03721b50817002c35
John Fediaczko <jf2751@g.rit.edu>
2015-11-02 17:32:03 -0500

    Added the AddBox() method to BOmanager.

--- E13_BoundingBoxClass/BoundingObjectManager.h
+++ E13_BoundingBoxClass/BoundingObjectManager.h
@@ -1,18 +1,25 @@
 #pragma once
 
 #include "RE\ReEngAppClass.h"
+#include "BoundingObject.h"
 
 class BoundingObjectManager
 {
 	
 private:
 	BoundingObjectManager();
-	~BoundingObjectManager();
+	std::vector<BoundingObject> boundingObjList;
 public:
 	static BoundingObjectManager* instance;
 	static BoundingObjectManager* GetInstance();
 	static void Release();
-	
-	
+	void AddBox(std::vector<vector3> verticies);
+	void SwitchBoxVisibility();
+	void GetBoundingObjNumber();
+	void SetColor();
+	void SetVisibility();
+	void RenderSetting();
+	void CheckCollisions();
+
 };
 
